525cb7648ab97c06e135d7290414300d4b90a6e2
Paul <paul@graffino.com>
2019-08-21 16:57:09 +0300

    Add postcss

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require("css-mqpacker")(),
+    require('cssnano')({
+      preset: 'default'
+    }),
+    purgecss({
+      content: ['./**/*.handlebars'],
+      css: ['./**/*.scss'],
+      whitelistPatterns: [/^is-/, /^has-/, /^animation-/],
+      whitelistPatternsChildren: [],
+      keyframes: true
+    })
+  ]
+}
