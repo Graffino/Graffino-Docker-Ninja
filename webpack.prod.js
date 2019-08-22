9176ea78bd1947fbfb7b1a3ed04568f8c051914f
Paul <paul@graffino.com>
2019-08-22 11:29:21 +0300

    Add favicon generation

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,22 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.config.js');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   optimization: [],
-  plugins: []
+  plugins: [
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, 'src/favicon.svg'),
+      icons: {
+        twitter: true,
+        windows: true
+      },
+      inject: true,
+      background: '#fff',
+      title: 'Graffino Ninja',
+    })
+  ]
 });
