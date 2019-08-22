2cc957e76077bf8359c40ef71b62cf7e5a79fc23
Paul <paul@graffino.com>
2019-08-22 11:45:31 +0300

    Add base and partials

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.config.js');
@@ -5,6 +6,16 @@ const common = require('./webpack.config.js');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    liveReload: true,
+    http2: false,
+    https: false,
+    host: 'localhost',
+    port: 4200,
+    hot: true,
+  },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ]
