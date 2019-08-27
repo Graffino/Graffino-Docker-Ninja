572938b98a2751a4a189e9bfa2d4b346fc04ff6f
Paul <paul@graffino.com>
2019-08-27 15:49:43 +0300

    Remove legacy module import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import '@babel/polyfill'
 import requireAll from './utils/require-all'
 import autoInitComponents from './utils/auto-init-components'
 
-requireAll(require.context('./modules/', true, /\.js$/))
 requireAll(require.context('./images/', true, /\.(png|svg|jpe?g|gif)$/))
 requireAll(require.context('./icons/', true, /\.svg$/))
 
