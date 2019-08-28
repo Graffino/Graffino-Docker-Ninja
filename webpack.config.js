7cb6de947806bb4685461bc53a33e53e35ad55c0
Paul <paul@graffino.com>
2019-08-28 11:17:53 +0300

    Add js global variables

--- webpack.config.js
+++ webpack.config.js
@@ -123,10 +123,6 @@ module.exports = {
       }
     ]
   },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyPlugin([{
