0c6c610def7082f24082050ea38b81c6b3c9df7b
Zeno Popovici <zeno@graffino.com>
2019-09-06 17:17:36 +0300

    Fix database creation

--- wordpress/setup-wordpress.js
+++ wordpress/setup-wordpress.js
@@ -5,6 +5,12 @@ const themeDatabase = process.env.THEME_DB
 const DatabaseHost = process.env.DB_HOST
 const DatabaseUser = process.env.DB_USER
 const DatabasePassword = process.env.DB_PASSWORD
+const app = process
+
+if (!themeDatabase || !DatabaseHost || !DatabaseUser) {
+  console.log('Configuration values not set. Please edit your .env file!')
+  app.exit();
+}
 
 const databaseConnect = () => {
   const pool = mariadb.createPool({
@@ -17,21 +23,18 @@ const databaseConnect = () => {
     .then(connection => {
       connection.query(`CREATE DATABASE ${themeDatabase}`)
         .then((res) => {
-          console.log(res)
+          console.log(`Database ${themeDatabase} created!`)
           connection.end()
+          app.exit()
         })
         .catch(err => {
-          console.log(err)
-          connection.end()
+          console.log(`Database ${themeDatabase} already exists or incorrect permisions!`)
+          app.exit()
         })
     }).catch(err => {
-      console.log(err)
+      console.log(`Cannot connect to database. Reason: ${err.code}`)
+      app.exit()
     })
 }
 
-const run = async () => {
-  databaseConnect()
-  process.exit()
-}
-
-run()
+databaseConnect()
