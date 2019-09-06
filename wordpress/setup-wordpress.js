eea2bad8a49282bb34615bf767938677ecccdef8
Paul <paul@graffino.com>
2019-09-06 16:26:49 +0300

    Remove hardcoded creds

--- wordpress/setup-wordpress.js
+++ wordpress/setup-wordpress.js
@@ -2,12 +2,15 @@ const mariadb = require('mariadb')
 require('dotenv').config()
 
 const themeDatabase = process.env.THEME_DB
+const DatabaseHost = process.env.DB_HOST
+const DatabaseUser = process.env.DB_USER
+const DatabasePassword = process.env.DB_PASSWORD
 
 const databaseConnect = () => {
   const pool = mariadb.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: ''
+    host: DatabaseHost,
+    user: DatabaseUser,
+    password: DatabasePassword
   })
 
   pool.getConnection()
