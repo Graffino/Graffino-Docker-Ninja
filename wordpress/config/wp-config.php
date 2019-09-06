e8b0df080cb2534daa15356254e9925c8ae4e42d
Zeno Popovici <zeno@graffino.com>
2019-09-06 19:18:06 +0300

    Add WP Debug

--- wordpress/config/wp-config.php
+++ wordpress/config/wp-config.php
@@ -1,6 +1,6 @@
 <?php
-require_once(__DIR__ . '/../composer/vendor/autoload.php');
-$dotenv = Dotenv\Dotenv::create(dirname(__DIR__, 1));
+require_once( __DIR__ . '/../composer/vendor/autoload.php' );
+$dotenv = Dotenv\Dotenv::create( dirname( __DIR__, 1 ) );
 $dotenv->load();
 
 /**
@@ -23,22 +23,22 @@ $dotenv->load();
  */
 
 
-define( 'WP_HOME', getenv('THEME_DEV_URL') );
-define( 'WP_SITEURL', getenv('THEME_DEV_URL') );
-define( 'WP_CONTENT_URL', getenv('THEME_DEV_URL') . '/wp-content' );
+define( 'WP_HOME', getenv( 'THEME_DEV_URL' ) );
+define( 'WP_SITEURL', getenv( 'THEME_DEV_URL' ) );
+define( 'WP_CONTENT_URL', getenv( 'THEME_DEV_URL' ) . '/wp-content' );
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('THEME_DB') );
+define( 'DB_NAME', getenv( 'THEME_DB' ) );
 
 /** MySQL database username */
-define( 'DB_USER', getenv('DB_USER') );
+define( 'DB_USER', getenv( 'DB_USER' ) );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv('DB_HOST') );
+define( 'DB_HOST', getenv( 'DB_HOST' ) );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -55,14 +55,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         ',5d_~G~BoiAt(c+t9jk2z|`,50#<U-)dIqAKo(N<Or,Er=`UbsJk.kTe<dY04=X%');
-define('SECURE_AUTH_KEY',  'S EVKqi-P^(bZj1[u .3ZFI(}p<c[}w..kF<8;3AQ;9s&8B3QsC,y|<7_e6+s5ke');
-define('LOGGED_IN_KEY',    'i]|~lCLN;*]kGa$1b)PBMAbU>$<lmPRUy,*uUWVPqQU_W#A-FtXu?<ftrg#9)rHw');
-define('NONCE_KEY',        '-uS.bEBtP9k@>?Pn++!Pk#mAhH]^3~Y$K+qWvWlO+c_>^S##K4+nb$|0XOpViB+p');
-define('AUTH_SALT',        '/B,G:2lJQo|SQB=TI$YEnbl4hci*T<bsosTeR$B)BgN%TWl_3t?SoLZWW+_+~|`p');
-define('SECURE_AUTH_SALT', ')nuZAr%+K1ob-EDd^CH`rUNKW|l,Cu2]bW74q,p/E#KbmvO*y(<>_sUsFR]Y8)d.');
-define('LOGGED_IN_SALT',   'KQ+!!,0|0XN%(*9*26k-Jk>MZ|[~35zR3+Q,,#>5.:Bod_a;HzGMKs+E8%AAE[_$');
-define('NONCE_SALT',       '@ShGL3lc<W=QIm3}=Oh*[8_}8i;,+)S5T/V_/H08!mo=#eiGQ;!VC;|d?`VpyOyT');
+define( 'AUTH_KEY', ',5d_~G~BoiAt(c+t9jk2z|`,50#<U-)dIqAKo(N<Or,Er=`UbsJk.kTe<dY04=X%' );
+define( 'SECURE_AUTH_KEY', 'S EVKqi-P^(bZj1[u .3ZFI(}p<c[}w..kF<8;3AQ;9s&8B3QsC,y|<7_e6+s5ke' );
+define( 'LOGGED_IN_KEY', 'i]|~lCLN;*]kGa$1b)PBMAbU>$<lmPRUy,*uUWVPqQU_W#A-FtXu?<ftrg#9)rHw' );
+define( 'NONCE_KEY', '-uS.bEBtP9k@>?Pn++!Pk#mAhH]^3~Y$K+qWvWlO+c_>^S##K4+nb$|0XOpViB+p' );
+define( 'AUTH_SALT', '/B,G:2lJQo|SQB=TI$YEnbl4hci*T<bsosTeR$B)BgN%TWl_3t?SoLZWW+_+~|`p' );
+define( 'SECURE_AUTH_SALT', ')nuZAr%+K1ob-EDd^CH`rUNKW|l,Cu2]bW74q,p/E#KbmvO*y(<>_sUsFR]Y8)d.' );
+define( 'LOGGED_IN_SALT', 'KQ+!!,0|0XN%(*9*26k-Jk>MZ|[~35zR3+Q,,#>5.:Bod_a;HzGMKs+E8%AAE[_$' );
+define( 'NONCE_SALT', '@ShGL3lc<W=QIm3}=Oh*[8_}8i;,+)S5T/V_/H08!mo=#eiGQ;!VC;|d?`VpyOyT' );
 
 /**#@-*/
 
@@ -86,7 +86,7 @@ $table_prefix = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', getenv( 'DEBUG' ) );
 
 /* That's all, stop editing! Happy publishing. */
 
