4a9a47fd1288f89930fd9bbcfc226e8cf5e55af9
Zeno Popovici <zeno@graffino.com>
2019-09-09 19:37:37 +0300

    Bugfixes

--- wordpress/scripts/setup.js
+++ wordpress/scripts/setup.js
@@ -1,13 +1,13 @@
 const mariadb = require('mariadb')
 require('dotenv').config()
 
-const themeDatabase = process.env.THEME_DB
+const DatabaseName = process.env.DB_NAME
 const DatabaseHost = process.env.DB_HOST
 const DatabaseUser = process.env.DB_USER
 const DatabasePassword = process.env.DB_PASSWORD
 const app = process
 
-if (!themeDatabase || !DatabaseHost || !DatabaseUser) {
+if (!DatabaseName || !DatabaseHost || !DatabaseUser) {
   console.log('Configuration values not set. Please edit your .env file!')
   app.exit();
 }
@@ -21,14 +21,14 @@ const databaseConnect = () => {
 
   pool.getConnection()
     .then(connection => {
-      connection.query(`CREATE DATABASE \`${themeDatabase}\``)
+      connection.query(`CREATE DATABASE \`${DatabaseName}\``)
         .then((res) => {
-          console.log(`Database ${themeDatabase} created!`)
+          console.log(`Database ${DatabaseName} created!`)
           connection.end()
           app.exit()
         })
         .catch(err => {
-          console.log(`Database ${themeDatabase} already exists or incorrect permisions! Error: ${err.code}`)
+          console.log(`Database ${DatabaseName} already exists or incorrect permisions! Error: ${err.code}`)
           app.exit()
         })
     }).catch(err => {
