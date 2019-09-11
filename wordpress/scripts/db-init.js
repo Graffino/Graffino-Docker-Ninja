5664f8749573d5d67d413d3b288a3e4cf9a9feee
Zeno Popovici <zeno@graffino.com>
2019-09-11 03:36:42 +0300

    Bugfixes
    Remove obsolete file
    
    Fixes

--- wordpress/scripts/db-init.js
+++ wordpress/scripts/db-init.js
@@ -1,40 +1,59 @@
+// Require modules
 const mariadb = require('mariadb')
-require('dotenv').config()
 
+// Get vars from env
+require('dotenv').config()
 const DatabaseName = process.env.DB_NAME
 const DatabaseHost = process.env.DB_HOST
 const DatabaseUser = process.env.DB_USER
 const DatabasePassword = process.env.DB_PASSWORD
+
+// Save app as process
 const app = process
 
+// Check if DB connection data is set
 if (!DatabaseName || !DatabaseHost || !DatabaseUser) {
   console.log('Configuration values not set. Please edit your .env file!')
-  app.exit();
+  terminate();
 }
 
-const databaseConnect = () => {
+// Start
+const start = () => {
+  // Connect to database
   const pool = mariadb.createPool({
     host: DatabaseHost,
     user: DatabaseUser,
     password: DatabasePassword
   })
 
+  // Create database
   pool.getConnection()
     .then(connection => {
       connection.query(`CREATE DATABASE \`${DatabaseName}\``)
         .then((res) => {
           console.log(`Database ${DatabaseName} created!`)
+          // Close connection
           connection.end()
-          app.exit()
+          // Run terminate()
+          terminate()
         })
         .catch(err => {
           console.log(`Database ${DatabaseName} already exists or incorrect permisions! Error: ${err.code}`)
-          app.exit()
+          // Run terminate()
+          terminate()
         })
     }).catch(err => {
       console.log(`Cannot connect to database. Error: ${err.code}`)
-      app.exit()
+      // Run terminate()
+      terminate()
     })
 }
 
-databaseConnect()
+// Terminate app
+const terminate = () => {
+  console.log(`Temporary files deleted.`)
+  // Exit app
+  app.exit()
+}
+
+start()
