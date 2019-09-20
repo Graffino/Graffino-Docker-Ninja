780a5ac09e48d13c320860cc51f167c3688885c1
Daniel Gomonea <daniel@graffino.com>
2019-09-20 16:06:08 +0300

    Implement multi data-component per element

--- src/scripts/utils/auto-init-components.js
+++ src/scripts/utils/auto-init-components.js
@@ -2,13 +2,24 @@ import moduleLoader from './module-loader'
 
 const autoInitComponents = () => {
   Array.from(document.querySelectorAll('[data-component]')).map(element => {
-    const componentName = element.dataset.component
-    moduleLoader(componentName).then(Component => {
-      // eslint-disable-next-line no-unused-vars
-      const component = new Component({
-        element
+    const componentName = element.dataset.component.split(' ')
+    if (componentName.length === 1) {
+      moduleLoader(componentName[0]).then(Component => {
+        // eslint-disable-next-line no-unused-vars
+        const compontent = new Component({
+          element
+        })
       })
-    })
+    } else {
+      componentName.map(elem => {
+        moduleLoader(elem).then(Component => {
+          // eslint-disable-next-line no-unused-vars
+          const compontent = new Component({
+            element
+          })
+        })
+      })
+    }
   })
 }
 
