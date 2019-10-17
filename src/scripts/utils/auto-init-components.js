7cdb4e8eeee4884186f5f31501c6d0028292ecaa
Daniel Gomonea <daniel@graffino.com>
2019-10-17 16:23:40 +0300

    Adds prop import

--- src/scripts/utils/auto-init-components.js
+++ src/scripts/utils/auto-init-components.js
@@ -3,19 +3,30 @@ import moduleLoader from './module-loader'
 const autoInitComponents = () => {
   Array.from(document.querySelectorAll('[data-component]')).map(element => {
     const componentName = element.dataset.component.split(' ')
+    let props = []
+
+    Object.keys(element.dataset)
+      .filter(prop => prop.includes('prop'))
+      .map(key => {
+        const propKey = `${key.replace('prop', '').charAt(0).toLowerCase()}${key.replace('prop', '').substring(1)}`
+        props = { ...props, [`${propKey}`]: element.dataset[key] }
+      })
+
     if (componentName.length === 1) {
       moduleLoader(componentName[0]).then(Component => {
-        // eslint-disable-next-line no-unused-vars
-        const compontent = new Component({
-          element
+        // eslint-disable-next-line no-new
+        new Component({
+          element,
+          ...props
         })
       })
     } else {
       componentName.map(elem => {
         moduleLoader(elem).then(Component => {
-          // eslint-disable-next-line no-unused-vars
-          const compontent = new Component({
-            element
+          // eslint-disable-next-line no-new
+          new Component({
+            element,
+            ...props
           })
         })
       })
