7682ef411dd08207fc506e7c6c3921517d506c76
Adina Badila <48824233+adinabadila@users.noreply.github.com>
2019-12-18 14:53:50 +0200

    Adds dropdown feature (#21)

---
+++ src/scripts/modules/dropdown.js
@@ -0,0 +1,39 @@
+import Component from '../utils/component.js'
+import { dom } from '../utils/globals.js'
+
+export default class Dropdown extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { ...props }
+  }
+
+  init () {
+    this.handler()
+  }
+
+  handler () {
+    this.setState({
+      triggers: dom.document.querySelectorAll('.js-dropdown-trigger')
+    })
+
+    const { triggers } = this.state
+    for (const element of triggers) {
+      element.addEventListener('click', e => {
+        e.stopPropagation()
+        e.preventDefault()
+        this.setState({
+          current: element
+        })
+        this.open()
+      })
+    }
+  }
+
+  open () {
+    if (this.state.element.style.maxHeight) {
+      this.state.element.style.maxHeight = null
+    } else {
+      this.state.element.style.maxHeight = `${this.state.element.scrollHeight}px`
+    }
+  }
+}
