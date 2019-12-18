9791ef87fab0af2302dfe0b0b1f9db459839c01a
Adina Badila <48824233+adinabadila@users.noreply.github.com>
2019-12-18 14:50:24 +0200

    Adds new feature for header (#23)

---
+++ src/scripts/modules/header-pinned.js
@@ -0,0 +1,55 @@
+import Component from '../utils/component.js'
+import { dom } from '../utils/globals.js'
+
+export default class Header extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { ...props }
+  }
+
+  init () {
+    const data = {}
+    data.lastKnownScrollY = 0
+    data.currentScrollY = 0
+
+    this.setState({
+      data
+    })
+  }
+
+  onScroll () {
+    const { element, data } = this.state
+    const classes = {
+      pinned: 'is-pinned',
+      unpinned: 'is-unpinned'
+    }
+
+    data.currentScrollY = dom.window.pageYOffset
+    if (dom.window.innerWidth < 1050) {
+      if (data.currentScrollY > element.offsetHeight) {
+        if (data.currentScrollY < data.lastKnownScrollY) {
+          if (element.classList.contains(classes.unpinned)) {
+            element.classList.remove(classes.unpinned)
+            element.classList.add(classes.pinned)
+          }
+        } else if (data.currentScrollY > data.lastKnownScrollY) {
+          if (element.classList.contains(classes.pinned) || !element.classList.contains(classes.unpinned)) {
+            element.classList.remove(classes.pinned)
+            element.classList.add(classes.unpinned)
+          }
+        }
+        data.lastKnownScrollY = data.currentScrollY
+      }
+    }
+  }
+
+  onResize () {
+    if (dom.window.innerWidth > 1050) {
+      const { element } = this.state
+      const classes = {
+        unpinned: 'is-unpinned'
+      }
+      element.classList.remove(classes.unpinned)
+    }
+  }
+}
