bcd80a1a573728200ca3b7d0b2d147f9063ec6f4
Paul <paul@graffino.com>
2020-01-08 16:16:59 +0200

    Add babel plugins to config

--- webpack.config.js
+++ webpack.config.js
@@ -76,7 +76,8 @@ module.exports = {
       use: [{
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env']
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-transform-exponentiation-operator']
         }
       },
       'eslint-loader'
