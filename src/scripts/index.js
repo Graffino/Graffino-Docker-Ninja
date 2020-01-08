54a8bff9683a6614f0a6e5ec8c281c83facc6668
Paul <paul@graffino.com>
2020-01-08 15:50:24 +0200

    Add polyfill loader

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import '../styles/app.scss'
 import '@babel/polyfill'
-
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import loadPolyfills from 'polyfill-io-loader!?Promise,NodeList.prototype.forEach,Object.assign'
 import requireAll from './utils/require-all'
 import autoInitComponents from './utils/auto-init-components'
 
@@ -8,5 +9,7 @@ requireAll(require.context('../images/', true, /\.(png|svg|jpe?g|gif)$/))
 requireAll(require.context('../icons/', true, /\.svg$/))
 
 document.addEventListener('DOMContentLoaded', () => {
-  autoInitComponents()
+  loadPolyfills(() => {
+    autoInitComponents()
+  })
 })
