4dcb52b4deea87fddd7e114e1eece1853a778b1c
Zeno Popovici <zeno@graffino.com>
2020-01-20 18:14:20 +0200

    Remove unused legacy code

--- wordpress/theme/footer.php
+++ wordpress/theme/footer.php
@@ -14,10 +14,6 @@
 			</div>
 		</div><!-- .container -->
 	</footer>
-	<!-- Javascript -->
-	<script src="<?php bloginfo( 'template_directory' ); ?>/js/<?php asset_output( 'main', 'js' ); ?>" async></script>
-	<!-- /Javascript -->
-
 	<?php wp_footer(); ?>
 
 	<?php if ( is_debug() ) : ?>
