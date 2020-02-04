3e55769640d5f69d65ae63f18224eac8b92377be
Daniel Gomonea <daniel@graffino.com>
2020-02-04 17:33:29 +0200

    Rewrite images

--- wordpress/theme/inc/theme/classes/class-nwp-hooks.php
+++ wordpress/theme/inc/theme/classes/class-nwp-hooks.php
@@ -15,5 +15,8 @@ class NWP_Hooks {
 		$menus = $registry->get( 'NWP_Menus' );
 		add_action( 'init', array( $menus, 'nwp_register_menus' ) );
 		add_filter( 'nav_menu_css_class', array( $menus, 'current_type_nav_class' ), 10, 2 );
+
+		$images = $registry->get( 'NWP_Images' );
+		add_action( 'after_setup_theme', array( $images, 'nwp_images' ) );
 	}
 }
