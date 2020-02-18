471368ffa2d57d0388c476d35bc459cab57cce10
Paul <paul@graffino.com>
2020-02-18 13:15:44 +0200

    Avoid FOIT

---
+++ src/scripts/modules/avoid-foit.js
@@ -0,0 +1,23 @@
+import Component from '../utils/component'
+const FontFaceObserver = require('fontfaceobserver')
+
+export default class AvoidFOIT extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { ...props }
+  }
+
+  init () {
+    const fonts = ['Montserrat', 'Inconsolata']
+
+    fonts.forEach(font => {
+      const observer = new FontFaceObserver(font)
+
+      observer.load().then(() => {
+        this.state.element.classList.remove('no-fonts')
+      }, () => {
+        this.state.element.classList.add('no-fonts')
+      })
+    })
+  }
+}
