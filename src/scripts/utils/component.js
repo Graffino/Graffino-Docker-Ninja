48d2ae1f7dfab905204ad3546c6586f499cf69d6
Daniel Gomonea <daniel@graffino.com>
2020-02-24 11:51:33 +0200

    Revert "Remove onload event from init() method"
    This reverts commit 7eb17807bb7392992edc6af4c25d2b8e6e87c94c.

--- src/scripts/utils/component.js
+++ src/scripts/utils/component.js
@@ -17,9 +17,11 @@ class Component {
     this.onResize = this.onResize.bind(this)
     this.onScroll = this.onScroll.bind(this)
 
-    this.init()
-    dom.window.addEventListener('scroll', () => this.onScroll())
-    dom.window.addEventListener('resize', () => this.onResize())
+    dom.window.addEventListener('load', () => {
+      this.init()
+      dom.window.addEventListener('scroll', () => this.onScroll())
+      dom.window.addEventListener('resize', () => this.onResize())
+    })
   }
 
   setState (props) {
