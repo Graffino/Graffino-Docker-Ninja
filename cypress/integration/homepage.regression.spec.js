0fd5de82400d02e11e1e1c4e5a1a5956a75a42c6
Nick Ciolpan <nick@graffino.com>
2020-02-25 21:01:19 +0200

    moved to chrome

--- cypress/integration/homepage.regression.spec.js
+++ cypress/integration/homepage.regression.spec.js
@@ -2,13 +2,13 @@ context('Regression', () => {
   beforeEach(() => {
     cy.visit('/')
     cy.wait(5000)
-    cy.viewport(1280, 720)
   })
 
   describe('Homepage', () => {
     it('should have a nice looking button', () => {
       cy.get('.hero .button').matchImageSnapshot()
     })
+
     it('should have a nice looking homepage', () => {
       cy.get('body').matchImageSnapshot()
     })
