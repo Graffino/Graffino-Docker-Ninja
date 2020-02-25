67719a0e315cbbe59e028319c4f269ba625aa260
Nick Ciolpan <nick@graffino.com>
2020-02-25 16:27:16 +0200

    updated waiting time

--- cypress/integration/homepage.regression.spec.js
+++ cypress/integration/homepage.regression.spec.js
@@ -1,11 +1,11 @@
 context('Regression', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
+    cy.wait(5000)
   })
 
   describe('Homepage', () => {
     it('should have a nice looking button', () => {
-      cy.wait(5000)
       cy.get('.hero .button').matchImageSnapshot()
     })
   })
