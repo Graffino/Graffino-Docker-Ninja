4fcf3079423162104c0ece6955657020f78d0bb0
Nick Ciolpan <nick@graffino.com>
2020-02-25 20:05:36 +0200

    test again

--- cypress/integration/homepage.regression.spec.js
+++ cypress/integration/homepage.regression.spec.js
@@ -2,6 +2,7 @@ context('Regression', () => {
   beforeEach(() => {
     cy.visit('/')
     cy.wait(5000)
+    cy.viewport(1280, 720)
   })
 
   describe('Homepage', () => {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,9 +26,4 @@
 
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command'
 
-addMatchImageSnapshotCommand({
-  failureThreshold: 0.5, // threshold for entire image
-  failureThresholdType: 'percent', // percent of image or number of pixels
-  customDiffConfig: { threshold: 0.5 }, // threshold for each pixel
-  capture: 'viewport' // capture viewport in screenshot
-})
+addMatchImageSnapshotCommand()
