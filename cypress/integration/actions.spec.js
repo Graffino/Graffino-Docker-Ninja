59a5a02ba004e5cd8c6e35d242fc7090eb40a6b7
Nick Ciolpan <nick@graffino.com>
2020-02-25 13:19:10 +0200

    Removed comments from tests

--- cypress/integration/actions.spec.js
+++ cypress/integration/actions.spec.js
@@ -1,11 +1,7 @@
-/// <reference types="cypress" />
-
 context('Actions', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
-
-  // https://on.cypress.io/interacting-with-elements
 
   it('Redirects down below', () => {
     cy.get('.hero .button').click()
--- cypress/integration/regression/homepage.spec.js
+++ cypress/integration/regression/homepage.spec.js
@@ -1,19 +1,11 @@
-/// <reference types="cypress" />
-
 context('Regression', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
 
-  // https://on.cypress.io/interacting-with-elements
   describe('Homepage', () => {
     it('should look nice', () => {
-
-      // snapshot name will be the test title
       cy.wait(5000)
-
-      // cy.matchImageSnapshot()
-      // match element snapshot
     })
     it('should have a nice looking button', () => {
       cy.get('.hero .button').matchImageSnapshot()
