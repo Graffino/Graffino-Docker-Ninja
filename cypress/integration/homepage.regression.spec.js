2be5ed0ed0a6da626066775b2079ba46ee9b4bb4
Nick Ciolpan <nick@graffino.com>
2020-02-25 16:21:51 +0200

    test test

--- cypress/integration/homepage.regression.spec.js
+++ cypress/integration/homepage.regression.spec.js
@@ -4,9 +4,6 @@ context('Regression', () => {
   })
 
   describe('Homepage', () => {
-    it('should look nice', () => {
-      cy.wait(5000)
-    })
     it('should have a nice looking button', () => {
       cy.get('.hero .button').matchImageSnapshot()
     })
