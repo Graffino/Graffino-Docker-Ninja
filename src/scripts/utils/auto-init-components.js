5db964ff3a7a9a991d1cb700a93f5d49e6cccc18
Nick Ciolpan <nick@graffino.com>
2020-05-05 20:48:51 +0300

    Added refs (#38)
    * Added refs dataset
    
    * Added test refs; namescaped props and refs;
    
    * Array / unique ref handling

--- src/scripts/utils/auto-init-components.js
+++ src/scripts/utils/auto-init-components.js
@@ -1,9 +1,16 @@
 import moduleLoader from './module-loader'
 
+const camelCase = function (string) {
+  return string.replace(/-([a-z])/g, function (g) {
+    return g[1].toUpperCase()
+  })
+}
+
 const autoInitComponents = () => {
   Array.from(document.querySelectorAll('[data-component]')).map((element) => {
     const componentName = element.dataset.component.split(' ')
-    let props = []
+    let props = {}
+    let refs = {}
 
     Object.keys(element.dataset)
       .filter((prop) => prop.includes('prop'))
@@ -15,12 +22,30 @@ const autoInitComponents = () => {
         props = { ...props, [`${propKey}`]: element.dataset[key] }
       })
 
+    Array.from(element.querySelectorAll('[data-ref]'))
+      .map(el => {
+        // eslint-disable-next-line no-prototype-builtins
+        if (refs.hasOwnProperty(camelCase(el.dataset.ref))) {
+          refs = { ...refs, [`${camelCase(el.dataset.ref)}`]: [...refs[`${camelCase(el.dataset.ref)}`], el] }
+        } else {
+          refs = { ...refs, [`${camelCase(el.dataset.ref)}`]: [el] }
+        }
+      })
+
+    Object.keys(refs)
+      .map(key => {
+        if (refs[key].length === 1) {
+          refs[key] = refs[key][0]
+        }
+      })
+
     if (componentName.length === 1) {
       moduleLoader(componentName[0]).then((Component) => {
         // eslint-disable-next-line no-new
         new Component({
           element,
-          ...props
+          props,
+          refs
         })
       })
     } else {
@@ -29,7 +54,8 @@ const autoInitComponents = () => {
           // eslint-disable-next-line no-new
           new Component({
             element,
-            ...props
+            props,
+            refs
           })
         })
       })
