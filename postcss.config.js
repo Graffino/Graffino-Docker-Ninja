715a846de1c5ebcc877906d70b59c7655be1d421
Zeno Popovici <zeno@graffino.com>
2020-09-21 18:50:57 +0300

    Fix postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,20 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
-    require('css-mqpacker')(),
-    require('postcss-sorting'),
-    require('cssnano')({
-      preset: [
-        'default',
-        {
-          discardComments: {
-            removeAll: true
+    'autoprefixer',
+    ['css-mqpacker', ''],
+    'postcss-sorting',
+    [
+      'cssnano',
+      {
+        preset: [
+          'default',
+          {
+            discardComments: {
+              removeAll: true
+            }
           }
-        }
-      ]
-    })
+        ]
+      }
+    ]
   ]
 }
