022691137897e7864064d9c365c651084fe45e36
Zeno Popovici <zeno@graffino.com>
2020-10-12 19:59:19 +0300

    Bugfixes

--- webpack.prod.js
+++ webpack.prod.js
@@ -65,7 +65,7 @@ module.exports = merge(common, {
   optimization: {
     minimizer: [
       new OptimizeCSSAssetsPlugin({
-        assetNameRegExp: /^((?!style).)*\.css$/g
+        assetNameRegExp: /^(?!style|global).*\.css$/g
       }),
       new TerserPlugin({
         test: /\.js(\?.*)?$/i,
