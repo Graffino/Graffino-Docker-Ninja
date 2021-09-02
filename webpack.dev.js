a5eb0c579a2d79d6dce6afa2ef2f38a7f134f7e0
Vlad Craciun <vlad@graffino.com>
2021-09-02 13:52:58 +0300

    Feature/package updates 020921 (#81)
    * chore: update composer lock - MNT-337
    
    * chore: update node packages - MNT-337
    
    * chore: update webpack dev server - MNT-337
    
    * chore: update svgo - MNT-337
    
    * chore: update readme - MNT-337
    
    * fix: contentBase in devServer - MNT-337

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,7 +9,9 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
     compress: true,
     hot: true,
     liveReload: true,
@@ -17,27 +19,31 @@ module.exports = merge(common, {
     https: false,
     host: '127.0.0.1',
     port: 3000,
-    overlay: {
-      warnings: false,
-      errors: true
+    client: {
+      overlay: {
+        warnings: false,
+        errors: true
+      }
     },
-    stats: {
-      colors: true,
-      hash: false,
-      version: false,
-      timings: true,
-      assets: false,
-      chunks: false,
-      modules: false,
-      reasons: false,
-      children: false,
-      source: false,
-      errors: true,
-      errorDetails: false,
-      warnings: true,
-      publicPath: false
+    devMiddleware: {
+      stats: {
+        colors: true,
+        hash: false,
+        version: false,
+        timings: true,
+        assets: false,
+        chunks: false,
+        modules: false,
+        reasons: false,
+        children: false,
+        source: false,
+        errors: true,
+        errorDetails: false,
+        warnings: true,
+        publicPath: false
+      }
     },
-    disableHostCheck: true
+    allowedHosts: 'all'
   },
   module: {
     rules: [
@@ -74,7 +80,6 @@ module.exports = merge(common, {
       }
     ]
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: devMode ? 'js/[name].js' : 'js/[name].[fullhash].js',
