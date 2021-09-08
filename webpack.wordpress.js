d08ca6570114a301622052a0275f58e6e5647ec4
Chirita Roxana <roxana@graffino.com>
2021-09-08 12:57:30 +0300

    fix: remove svgo params

--- webpack.wordpress.js
+++ webpack.wordpress.js
@@ -160,16 +160,7 @@ module.exports = {
           name: 'sprite.svg',
           keep: true
         },
-        svgo: {
-          plugins: [
-            {
-              name: 'removeAttrs',
-              params: {
-                attrs: 'path:fill'
-              }
-            }
-          ]
-        }
+        svgo: {}
       },
       sprite: {
         prefix: false
